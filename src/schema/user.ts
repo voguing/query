189bc5bc3e4f1f3ab0bd9ab29cc322e937868e94
xleojs <xleo.js@gmail.com>
2024-08-07 01:32:12 +0800

    feat: 更新数据库

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -5,7 +5,7 @@ export const User = objectType({
   definition(t) {
     t.nonNull.int('id')
     t.string('name')
-    t.nonNull.string('username')
+    t.string('username')
     t.string('phone')
     t.string('avatar')
     t.string('openid')
